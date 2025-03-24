977093c0fe025ea9088668c1c439283353953925
TasneemAhmed <49993791+TasneemAhmed@users.noreply.github.com>
2025-03-24 16:42:02 +0300

    Update Create_DB_Bronze_Schema.sql

--- Bronze_Layer/Create_DB_Bronze_Schema.sql
+++ Bronze_Layer/Create_DB_Bronze_Schema.sql
@@ -1,2 +1,7 @@
+/*
+The bronze layer in Medallion architecture includes raw data without any processing on it
+- The layer will have 3 tables raw_customer, raw_product, raw_orders
+- The data gets from the External stage which connects to AWS S3
+*/
 create database if not exists pacificretail_db;
 create schema if not exists bronze;
